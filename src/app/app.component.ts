b8d260892b045690ec3ea70adb6fb5bcf74feffe
Student <example@example.com>
2019-06-10 12:03:02 -0700

    add class and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  recipes: Recipe[] = [
+  new Recipe('Meatloaf', ['meat', 'loaf'], ['wash the meatloaf', 'eat the meatloaf', 'poop the meatloaf'])
+];
 }
